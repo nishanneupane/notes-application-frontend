c45b07cf4f21a92c3e7e694ae99052d5d20c8d1e
unknown <nishanneupane368@gmail.com>
2023-05-25 08:30:01 +0545

    updated for production

--- src/components/Note/GetById.jsx
+++ src/components/Note/GetById.jsx
@@ -29,7 +29,7 @@ const GetById = () => {
                                 <h5 className="card-title" style={{ fontWeight: "600", paddingBottom: "20px", fontStyle: "normal" }}>{note.noteTitle}</h5>
 
                                 <p className="card-text">{note.noteDescription}</p>
-                                <a href="#" className="btn btn-primary">Get More</a>
+                                <a href="https://railway.app/project/e1362146-ab8b-40e3-b123-01b5f26d1352/service/4f1bd80a-8775-41a0-8bcb-51f75c6ea620?id=9c3b24e9-cbe0-498f-8fe8-54b302d95604/" className="btn btn-primary">Get More</a>
                             </div>
                             <div className="card-footer text-muted">
                                 {note.createdDate}
